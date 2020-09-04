eafdde015723dacecc90be47e685dc4ced90dc70
Pedro Ferreira <pedro.ferreira@cern.ch>
2020-09-04 15:57:51 +0200

    Publish SCORM package

--- build.sh
+++ build.sh
@@ -14,3 +14,8 @@ python md_to_adapt.py course.yml --replace indico-user-docs .
 npx adapt install
 # Build actual HTML
 npx grunt build
+
+cd indico-user-docs
+export COURSE_HASH=$(git rev-parse --short HEAD)
+cd ../build
+zip indico-course-$(date "+%Y%m%d")-${COURSE_HASH}.zip *
